66e08165ce72c39ed17127078693a4591e7f267e
Linton Charles <lintoncharles2000@gmail.com>
2024-07-07 19:13:06 +1000

    Initialise iOS project

---
+++ psychbase-patientapp/psychbase-patientapp/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  psychbase-patientapp
+//
+//  Created by Linton Charles on 7/7/2024.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ psychbase-patientapp/psychbase-patientapp/psychbase_patientappApp.swift
@@ -0,0 +1,17 @@
+//
+//  psychbase_patientappApp.swift
+//  psychbase-patientapp
+//
+//  Created by Linton Charles on 7/7/2024.
+//
+
+import SwiftUI
+
+@main
+struct psychbase_patientappApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
