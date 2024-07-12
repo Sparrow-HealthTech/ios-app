ec359e8d52053fdcd87cfee6ab8047cf69e837ef
Linton Charles <lintoncharles2000@gmail.com>
2024-07-12 17:30:14 +1000

    Complete Header

--- psychbase-patientapp/psychbase-patientapp/HeaderView.swift
+++ psychbase-patientapp/psychbase-patientapp/HeaderView.swift
@@ -9,30 +9,12 @@ import SwiftUI
 
 struct HeaderView: View {
     var body: some View {
-        VStack(){
-            HStack{
-                Rectangle()
-                    .foregroundColor(.clear)
-                    .frame(height: 35)
-                    .background(
-                        Image("sparrow-logo")
-                            .resizable()
-                            .aspectRatio(contentMode: .fill)
-                            .frame(width: 46.022727966308594, height: 45)
-                            .clipped()
-                            .padding(.leading, 240)
-                    )
-                Text("PSYCHBASE")
-                    .font(
-                        Font.custom("Quicksand", size: 20)
-                            .weight(.bold)
-                    )
-                    .multilineTextAlignment(.center)
-                    .foregroundColor(Color(red: 0.18, green: 0.16, blue: 0.17))
-                    .frame(width: 361, alignment: .center)
-                    .padding(.top, 5)
-            }
-            .frame(maxWidth: .infinity)
+        HStack() {
+            Image("psychbase-logo")
+                .resizable()
+                .frame(width: 140, height: 30)
+                .padding(.leading, 20)
+            Spacer()
         }
     }
 }
