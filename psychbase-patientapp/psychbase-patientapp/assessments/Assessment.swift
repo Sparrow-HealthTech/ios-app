f88bfa915a66106e69b90fdac2bf400645d1222e
Linton Charles <lintoncharles2000@gmail.com>
2024-07-31 00:31:20 +1000

    feat: Finish Assessments module

--- psychbase-patientapp/psychbase-patientapp/assessments/Assessment.swift
+++ psychbase-patientapp/psychbase-patientapp/assessments/Assessment.swift
@@ -10,20 +10,43 @@ import SwiftUI
 struct Assessment: View {
     @Binding var path: [appPages]
     
+    func completeAssessment() {
+        path.removeLast()
+    }
+    
     var body: some View {
         VStack {
             ModuleHeader(path: $path,
-                             headerTitle: "DASS-10 Assessment")
-            VStack(alignment: .leading, spacing: 0){
-                Divider()
-                VStack(alignment: .leading, spacing: 0) {
-                    Doctor()
-                    Questionnaire()
+                         headerTitle: "DASS-10 Assessment")
+            ScrollView {
+                VStack(alignment: .leading, spacing: 0){
+                    Divider()
+                    VStack(alignment: .leading, spacing: 0) {
+                        Doctor()
+                        Questionnaire()
+                    }
                 }
+                .frame(maxWidth: .infinity, alignment: .topLeading)
+                .background(Color(red: 0.95, green: 0.95, blue: 0.95))
+                
+                // Finish Assessment
+                HStack(alignment: .center, spacing: 12) {
+                    Button(action: completeAssessment) {
+                        Text("Finish Exercise!")
+                            .font(
+                                Font.custom("Quicksand", size: 16)
+                                    .weight(.bold)
+                            )
+                            .foregroundColor(.white)
+                    }
+                    .padding(.vertical, 10)
+                    .frame(maxWidth: .infinity, alignment: .center)
+                    .background(Color(red: 0.17, green: 0.63, blue: 0.28))
+                    .cornerRadius(12)
+                }
+                .padding(.horizontal, 30)
+                .frame(maxWidth: .infinity, alignment: .center)
             }
-            .frame(maxWidth: .infinity, alignment: .topLeading)
-            .background(Color(red: 0.95, green: 0.95, blue: 0.95))
-            Spacer()
         }
         .navigationBarBackButtonHidden(true)
     }
